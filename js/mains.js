b66661e77536764c10523dd7ad2f6684545ab0c4
MaxNumerique <maximilienlandois@gmail.com>
2024-04-30 15:50:24 +0200

    presque fini

--- js/mains.js
+++ js/mains.js
@@ -77,4 +77,30 @@ link.addEventListener('click', function(event) {
 });
 
 
+// ==== CAROUSEL ACCUEIL =====
 
+const carouselItems = document.querySelectorAll('.carouselExo');
+let currentSlide = 0;
+
+function showSlide(index) {
+    carouselItems.forEach((item, i) => {
+        if (i === index) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
+function nexSlide() {
+    currentSlide = (currentSlide + 1) % carouselItems.length;
+    showSlide(currentSlide);
+}
+
+function previousSlide() {
+    currentSlide = (currentSlide - 1 + carouselItems.length) % carouselItems.length;
+    showSlide(currentSlide);
+}
+
+document.querySelector('.prev-btn').addEventListener('click', previousSlide);
+document.querySelector('.next-btn').addEventListener('click', nexSlide);
