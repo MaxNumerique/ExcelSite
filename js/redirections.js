ea3d1a2725133941d1a5e2c2ef37d592fb723882
MaxNumerique <maximilienlandois@gmail.com>
2024-08-07 12:07:46 +0200

    test

--- js/redirections.js
+++ js/redirections.js
@@ -1,4 +1,4 @@
-import { coursContent } from './coursContent.js';
+import {coursContent} from './coursContent.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     function redirectToRandomCourse(event, specificCourses, expandAll = false) {
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Ajout d'un délai pour s'assurer que les données sont stockées
         setTimeout(() => {
-            window.location.href = 'cours.html';
+            window.location.href = 'cours';
         }, 100);
     }
 
